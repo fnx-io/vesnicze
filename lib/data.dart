2dfd229acf16eb346a1acc7dffc5a99f4226645f
Tomucha <tomucha@gmail.com>
2020-10-15 07:39:59 +0200

    aktualizace

--- lib/data.dart
+++ lib/data.dart
@@ -1,19 +1,19 @@
 var data = 
 {
-    "modified": "2020-10-14T18:01:22+02:00",
+    "modified": "2020-10-15T01:01:12+02:00",
     "source": "https:\/\/onemocneni-aktualne.mzcr.cz\/",
     "data": [
         {
-            "datum": "2020-10-14",
+            "datum": "2020-10-15",
             "provedene_testy_celkem": 1658331,
-            "potvrzene_pripady_celkem": 135425,
-            "aktivni_pripady": 73463,
-            "vyleceni": 60804,
-            "umrti": 1158,
+            "potvrzene_pripady_celkem": 139290,
+            "aktivni_pripady": 77217,
+            "vyleceni": 60901,
+            "umrti": 1172,
             "aktualne_hospitalizovani": 2678,
             "provedene_testy_vcerejsi_den": 30894,
-            "potvrzene_pripady_vcerejsi_den": 8325,
-            "potvrzene_pripady_dnesni_den": 5679
+            "potvrzene_pripady_vcerejsi_den": 9544,
+            "potvrzene_pripady_dnesni_den": 0
         }
     ]
 };
