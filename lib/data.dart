895a4d8a8b8842554a91869a50820fdde5b90a0b
Tomucha <tomucha@gmail.com>
2021-01-11 20:41:29 +0100

    Aktualizace dat

--- lib/data.dart
+++ lib/data.dart
@@ -1,19 +1,25 @@
 var data = 
 {
-    "modified": "2020-10-15T18:01:29+02:00",
+    "modified": "2021-01-11T07:59:28+01:00",
     "source": "https:\/\/onemocneni-aktualne.mzcr.cz\/",
     "data": [
         {
-            "datum": "2020-10-15",
-            "provedene_testy_celkem": 1693114,
-            "potvrzene_pripady_celkem": 144849,
-            "aktivni_pripady": 82126,
-            "vyleceni": 61505,
-            "umrti": 1218,
-            "aktualne_hospitalizovani": 2920,
-            "provedene_testy_vcerejsi_den": 34612,
-            "potvrzene_pripady_vcerejsi_den": 9544,
-            "potvrzene_pripady_dnesni_den": 5560
+            "datum": "2021-01-11",
+            "provedene_testy_celkem": 3848535,
+            "potvrzene_pripady_celkem": 835454,
+            "aktivni_pripady": 159753,
+            "vyleceni": 662429,
+            "umrti": 13272,
+            "aktualne_hospitalizovani": 6622,
+            "provedene_testy_vcerejsi_den": 11166,
+            "potvrzene_pripady_vcerejsi_den": 4283,
+            "potvrzene_pripady_dnesni_den": 0,
+            "provedene_testy_vcerejsi_den_datum": "2021-01-10",
+            "potvrzene_pripady_vcerejsi_den_datum": "2021-01-10",
+            "potvrzene_pripady_dnesni_den_datum": "2021-01-11",
+            "provedene_antigenni_testy_celkem": 1037481,
+            "provedene_antigenni_testy_vcerejsi_den": 9737,
+            "provedene_antigenni_testy_vcerejsi_den_datum": "2021-01-10"
         }
     ]
 };
